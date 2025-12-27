33cd6ccca05ab009808d1975709f4e53677ecacf
Neha Nupur <110279038+neha-nupur@users.noreply.github.com>
2025-12-27 21:08:09 +0530

    Time: 16 ms (48.84%) | Memory: 49.6 MB (81.88%) - LeetSync

--- 436-find-right-interval/find-right-interval.java
+++ 436-find-right-interval/find-right-interval.java
@@ -1,23 +1,66 @@
+// //Brute Force Solution
+
+// class Solution {
+//     public int[] findRightInterval(int[][] intervals) {
+//         int n = intervals.length;
+//         int[] result = new int[n];
+
+//         for(int i=0; i < n; i++){
+//             int end = intervals[i][1];
+//             int minStart = Integer.MAX_VALUE;
+//             int index = -1;
+
+//             for(int j=0; j<n; j++){
+//                 if(intervals[j][0] >= end) {
+//                     if(intervals[j][0] < minStart){
+//                         minStart = intervals[j][0];
+//                         index = j;
+//                     }
+//                 }
+//             }
+//             result[i] = index;
+//         }
+//         return result;
+//     }
+// }
+
+//Optimized Solution 
+
 class Solution {
     public int[] findRightInterval(int[][] intervals) {
         int n = intervals.length;
         int[] result = new int[n];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] starts = new int[n];
 
-        for(int i=0; i < n; i++){
-            int end = intervals[i][1];
-            int minStart = Integer.MAX_VALUE;
-            int index = -1;
-
-            for(int j=0; j<n; j++){
-                if(intervals[j][0] >= end) {
-                    if(intervals[j][0] < minStart){
-                        minStart = intervals[j][0];
-                        index = j;
-                    }
-                }
-            }
-            result[i] = index;
+        for (int i = 0; i < n; i++) {
+            starts[i] = intervals[i][0];
+            map.put(intervals[i][0], i);
         }
+
+        Arrays.sort(starts);
+
+        for (int i = 0; i < n; i++) {
+            int end = intervals[i][1];
+            int idx = lowerBound(starts, end);
+
+            if (idx == n) result[i] = -1;
+            else result[i] = map.get(starts[idx]);
+        }
+
         return result;
+    }
+
+    //binary search function
+    private int lowerBound(int[] arr, int target) {
+        int s = 0, e = arr.length - 1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (arr[mid] < target)
+                s = mid + 1;
+            else
+                e = mid - 1;
+        }
+        return s;
     }
 }
