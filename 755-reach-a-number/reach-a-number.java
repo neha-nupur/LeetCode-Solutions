c783262ff1eafc830e87acdc2b00e04285ab9a90
Neha Nupur <110279038+neha-nupur@users.noreply.github.com>
2025-12-28 23:28:32 +0530

    Time: 2 ms (58.23%) | Memory: 42.3 MB (13.29%) - LeetSync

--- 755-reach-a-number/reach-a-number.java
+++ 755-reach-a-number/reach-a-number.java
@@ -1,34 +1,13 @@
-// class Solution {
-//     public int reachNumber(int target) {
-//         target = Math.abs(target);   // symmetry
-//         int sum = 0;
-//         int step = 0;
-
-//         while (sum < target || (sum - target) % 2 != 0) {
-//             step++;
-//             sum += step;
-//         }
-//         return step;
-//     }
-// }
-
-
 class Solution {
     public int reachNumber(int target) {
-        target = Math.abs(target);
+        target = Math.abs(target);   // symmetry
+        int sum = 0;
+        int step = 0;
 
-        int k = (int) Math.ceil((Math.sqrt(1 + 8L * target) - 1) / 2);
-        int sum = k * (k + 1) / 2;
-
-        if ((sum - target) % 2 == 0) {
-            return k;
+        while (sum < target || (sum - target) % 2 != 0) {
+            step++;
+            sum += step;
         }
-
-        // parity fix
-        if ((k + 1) % 2 == 1) {
-            return k + 1;
-        } else {
-            return k + 2;
-        }
+        return step;
     }
 }
